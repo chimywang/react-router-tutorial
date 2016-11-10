26d544b43c8e7d6df0767e18adca5dfc1c7f6285
chimy <wangchengmin@wusongtech.com>
2016-11-10 16:54:28 +0800

    update

--- lessons/07-more-nesting/modules/Repos.js
+++ lessons/07-more-nesting/modules/Repos.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
-
+import NavLink from './NavLink'
 export default React.createClass({
   render() {
     return (
@@ -9,7 +9,10 @@ export default React.createClass({
         <ul>
           <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
           <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
+        <h2>{this.props.params.repoName}</h2>
       </div>
     )
   }
